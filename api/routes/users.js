7193cb57b548f7fb3516cfd0b7ae6dfd0efb170b
Kazachenko Viacheslav <kreeon@ya.ru>
2021-02-04 21:15:24 +0300

    msql

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,21 +1,38 @@
 const { Router } = require('express')
+const mysql = require('mysql2');
+
+const conn = mysql.createConnection({
+
+})
 
 const router = Router()
 
 // Mock Users
-const users = [
-  { name: 'Alexandre' },
-  { name: 'Pooya' },
-  { name: 'Sébastien' }
-]
+// const users = [
+//   { name: 'Alexandre' },
+//   { name: 'Pooya' },
+//   { name: 'Sébastien' }
+// ]
+let users = [];
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
-  res.json(users)
+  conn.query('SELECT * FROM users', function(err, data) {
+    if (err) {
+      res.writeHead(500);
+      res.end(err);
+    } else {
+      users = data;
+      res.json(data);
+      // res.end(JSON.stringify(data));
+    }
+  })
+  // res.json(users)
 })
 
 /* GET user by ID. */
 router.get('/users/:id', function (req, res, next) {
+  res.json(users)
   const id = parseInt(req.params.id)
   if (id >= 0 && id < users.length) {
     res.json(users[id])
